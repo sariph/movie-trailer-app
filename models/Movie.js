18976490dc434650e36813ee5ac4d192b90aeb1c
sariph <sariph.shrestha@gmail.com>
2017-06-18 18:41:43 -0400

    Update Movie.js

--- models/Movie.js
+++ models/Movie.js
@@ -8,7 +8,7 @@ var MovieSchema = new mongoose.Schema({
     },
     author: {
         type: String,
-        required: true
+        required: false
     },
     thumbnail: {
         type: String,
